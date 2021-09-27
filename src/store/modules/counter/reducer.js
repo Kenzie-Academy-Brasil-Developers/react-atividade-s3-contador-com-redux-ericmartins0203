9515d1bd29c3ab38bc822fd84378fa4499d9bc00
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-09-27 18:50:10 -0300

    feat actionTypes

--- src/store/modules/counter/reducer.js
+++ src/store/modules/counter/reducer.js
@@ -1,9 +1,11 @@
+import { ADD_NUMBER, SUB_NUMBER } from "./actionTypes";
+
 const reducerCounter = (state = 0, action) => {
   switch (action.type) {
-    case "ADD_NUMBER":
+    case ADD_NUMBER:
       return state + action.payload;
 
-    case "SUB_NUMBER":
+    case SUB_NUMBER:
       return state - action.payload;
 
     default:
